77d0fd18218431cd962031cea4530ef5ad2187d3
ajithittan <ajithittan@gmail.com>
2022-09-20 21:14:43 -0400

    Ok
    ok

---
+++ runstocksub.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const Redis = require("ioredis");
+console.log(process.env.REDIS_SERVER_PUBSUB)
+
+const redis = new Redis({
+  host: '192.168.1.210',
+  port: 6379,
+  username: 'sarphira-user',
+  password: 'SarPhira1290'
+});
+
+const messagesfromredis = []
+
+process.env.SUBSCRIBE_CHANNEL.split(',').map(eachChannel =>{
+    redis.subscribe(eachChannel, (err, count) => {
+        if (err) {
+          console.error("Failed to subscribe: %s", err.message);
+        } else {
+          console.log(
+            `Subscribed successfully! This client is currently subscribed to ${count} channels.`
+          );
+        }
+      });    
+}) 
+  
+redis.on("message", (channel, message) => {
+    console.log(`Received ${message} from ${channel}`);
+    messagesfromredis.push(message)
+});
+
+app.get("/",(req,res) =>{
+    res.send(messagesfromredis)
+})
+
+app.listen(process.env.PORT, () =>{
+    console.log("app is running on port",process.env.PORT)
+})
+
